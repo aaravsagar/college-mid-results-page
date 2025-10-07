d5156c5025c4365e89ac1f346881587553c4a378
Aarav Sagar <156703266+aaravsagar@users.noreply.github.com>
2025-10-07 11:43:05 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,22 +3,6 @@ function Footer() {
     <footer className="portal-footer">
       <div className="container">
         <div className="project-footer">PROJECT NEXUS</div>
-        
-        <div className="team-section">
-          <div className="team-members">
-            <h4>Team Members</h4>
-            <ul>
-              <li>244510316084 – Rishi Singh</li>
-              <li>244510316085 – Aarav Sagar</li>
-              <li>244510316086 – Arham Shah</li>
-            </ul>
-          </div>
-          
-          <div className="guide-section">
-            <h4>Project Guide</h4>
-            <p>Guided By Happy Mistry Sir & Palak Modi Mam</p>
-          </div>
-        </div>
       </div>
     </footer>
   );
